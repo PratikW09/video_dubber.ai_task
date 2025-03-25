c0a9ffc4d17baaf97987ab3730a5e2b0e6ae9394
Pratik09W <walalepratik09@gmail.com>
2025-03-25 17:18:05 +0530

    Implement the function and changes in nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true, // Add trailing slash for static exports
 };
 
-export default nextConfig;
+module.exports = nextConfig;
